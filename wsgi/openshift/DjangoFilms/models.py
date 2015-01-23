49d9f081c8c96085b060e6bfdf87c18da2aaaf5e
FranBarrera <franbarrera6@gmail.com>
2015-01-23 23:03:57 +0100

    añadir id_api a db

--- wsgi/openshift/DjangoFilms/models.py
+++ wsgi/openshift/DjangoFilms/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class media(models.Model):
+	id_api = models.IntegerField()
     type = models.IntegerField()
     name = models.CharField(max_length=100)
 #    mediauser = models.ManyToManyField(User, through='seguidas')
@@ -15,5 +16,7 @@ class usermedia(models.Model):
 	id_user = models.ForeignKey(User)
 	id_media = models.ForeignKey(media)
 	status = models.IntegerField()
+    class Meta:
+    	verbose_name_plural = 'media'
 
 #admin.site.register(media)
