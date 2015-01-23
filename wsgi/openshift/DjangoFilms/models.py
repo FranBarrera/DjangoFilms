bcdb71a2d42a6ff0dc60661ab937f2e5f1f59734
FranBarrera <franbarrera6@gmail.com>
2015-01-24 00:23:26 +0100

    quitar espacios

--- wsgi/openshift/DjangoFilms/models.py
+++ wsgi/openshift/DjangoFilms/models.py
@@ -4,19 +4,19 @@ from django.contrib.auth.models import User
 
 class media(models.Model):
 	id_api = models.IntegerField()
-    type = models.IntegerField()
-    name = models.CharField(max_length=100)
+	type = models.IntegerField()
+	name = models.CharField(max_length=100)
 #    mediauser = models.ManyToManyField(User, through='seguidas')
-    def __unicode__(self):
-    	return self.name
-    class Meta:
-    	verbose_name_plural = 'media'
+	def __unicode__(self):
+		return self.name
+	class Meta:
+		verbose_name_plural = 'media'
 
 class usermedia(models.Model):
 	id_user = models.ForeignKey(User)
 	id_media = models.ForeignKey(media)
 	status = models.IntegerField()
-    class Meta:
-    	verbose_name_plural = 'media'
+	class Meta:
+		verbose_name_plural = 'usermedia'
 
 #admin.site.register(media)
