a747f0b6a525b9a71b1cd313abd93b6ffa93784b
FranBarrera <franbarrera6@gmail.com>
2015-01-24 00:26:35 +0100

    añadir register

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'openshift.views.DjangoFilms/register'),
+    url(r'^register/$', 'openshift.DjangoFilms.views.register'),
 )
