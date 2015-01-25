231fdf6b98826212d2ef17e126f9b4cf9e2b340d
FranBarrera <franbarrera6@gmail.com>
2015-01-25 02:56:37 +0100

    crearar funcion movies_popular

--- wsgi/openshift/DjangoFilms/views.py
+++ wsgi/openshift/DjangoFilms/views.py
@@ -1,16 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 #from django.contrib import auth
 from django.contrib import auth
+import requests,json
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, "register.html", {
-        'form': form,
-    })
+ if request.method == 'POST':
+     form = UserCreationForm(request.POST)
+     if form.is_valid():
+         new_user = form.save()
+         return HttpResponseRedirect('/')
+ else:
+     form = UserCreationForm()
+ return render(request, "register.html", {
+     'form': form,
+ })
 
 def check(request):
     username = request.POST['username']
@@ -22,12 +29,23 @@ def check(request):
             request.session['username'] = username
             return HttpResponseRedirect('/')
         else:
-            return render(request, '2.html', {})
+            return HttpResponse('<p>Usuario inactivo</p>')
     else:
-        return render(request, '3.html', {})
+        return HttpResponse('<p>Login Incorrecto.<a href="/">Intentar de nuevo </a></p>')
 
 def login(request):
     if request.session.get("username"):
         return render(request, '1.html', {})
     else:
         return render(request, 'login.html', {})
+
+def movies_popular(user_token):
+        url = 'http://api.themoviedb.org/3/movie/popular'
+        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
+        request = requests.get(url,params=values)
+        resp = json.loads(request.text)
+        print resp
+        return render(request, "movies_popular.html", {
+            'data_raw': resp,
+            })
+
