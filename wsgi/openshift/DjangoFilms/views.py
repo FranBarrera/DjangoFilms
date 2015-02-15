07fc9bfa6d513d58e4b25c028776f56831e04a0e
FranBarrera <franbarrera6@gmail.com>
2015-02-15 22:24:26 +0100

    sql django devuelve id 5L en vez de 5

--- wsgi/openshift/DjangoFilms/views.py
+++ wsgi/openshift/DjangoFilms/views.py
@@ -57,10 +57,11 @@ def insert_media(request,api):
         return HttpResponse('<p>Insertada a media</p>')
 
 
-def vista(request,api):
-    username = request.session.get("username")
-    user_id = User.objects.filter(username=username).values('id')[0]['id']
-    media = models.media.objects.filter(api_id=api).values('id')[0]['id']
-    insert = models.usermedia(user=user_id,media=media,status=1)
-    insert.save()
-    return HttpResponse('<p>Insertada a vistas</p>')
+# def vista(request,api):
+#     username = request.session.get("username")
+#     user_id = User.objects.filter(username=username).values('id')[0]['id']
+#     print user_id
+#     media = models.media.objects.filter(api_id=api).values('id')[0]['id']
+#     insert = models.usermedia(user=user_id,media=media,status=1)
+#     insert.save()
+#     return HttpResponse('<p>Insertada a vistas</p>')
