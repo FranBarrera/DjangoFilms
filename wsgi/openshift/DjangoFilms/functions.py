fdee2974a6a3737278620d54d3a8de269d598e11
FranBarrera <franbarrera6@gmail.com>
2015-02-15 21:39:11 +0100

    crear fichero con funciones

---
+++ wsgi/openshift/DjangoFilms/functions.py
@@ -0,0 +1,8 @@
+import requests,json
+
+def info_peli(api):
+        url = 'http://api.themoviedb.org/3/movie/%s' %api
+        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
+        request = requests.get(url,params=values)
+        resp = json.loads(request.text)
+        return resp
