e41d19d71aec7443c0f41696a2d63232138fb02b
FranBarrera <franbarrera6@gmail.com>
2015-02-15 21:47:46 +0100

    mover función movies_popular a functions

--- wsgi/openshift/DjangoFilms/functions.py
+++ wsgi/openshift/DjangoFilms/functions.py
@@ -6,3 +6,10 @@ def info_peli(api):
         request = requests.get(url,params=values)
         resp = json.loads(request.text)
         return resp
+
+def movies_popular():
+        url = 'http://api.themoviedb.org/3/movie/popular'
+        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
+        request = requests.get(url,params=values)
+        resp = json.loads(request.text)
+        return resp
