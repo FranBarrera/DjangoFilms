7c8079f154afbff63984cbf12c3ca8f850b4454a
FranBarrera <franbarrera6@gmail.com>
2015-03-01 15:59:32 +0100

    corregir functions

--- wsgi/openshift/DjangoFilms/functions.py
+++ wsgi/openshift/DjangoFilms/functions.py
@@ -1,5 +1,7 @@
 import requests,json
 
+request_values =  {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'};
+
 
 def movies_popular():
         url = 'http://api.themoviedb.org/3/movie/popular'
