fd77aea587852ed4b90b74297cab16da7e1786e3
FranBarrera <franbarrera6@gmail.com>
2015-03-01 16:23:29 +0100

    eliminar lineas repetidas en functions

--- wsgi/openshift/DjangoFilms/functions.py
+++ wsgi/openshift/DjangoFilms/functions.py
@@ -5,15 +5,13 @@ request_values =  {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
 
 def movies_popular():
         url = 'http://api.themoviedb.org/3/movie/popular'
-        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
-        request = requests.get(url,params=values)
+        request = requests.get(url,params=request_values)
         resp = json.loads(request.text)
         return resp
 
 def info_peli(api):
         url = 'http://api.themoviedb.org/3/movie/%s' %api
-        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
-        request = requests.get(url,params=values)
+        request = requests.get(url,params=request_values)
         resp = json.loads(request.text)
         return resp
 
@@ -36,21 +34,18 @@ def cast_peli(api):
 
 def series_popular():
         url = 'http://api.themoviedb.org/3/tv/popular'
-        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
-        request = requests.get(url,params=values)
+        request = requests.get(url,params=request_values)
         resp = json.loads(request.text)
         return resp
 
 def info_serie(api):
         url = 'http://api.themoviedb.org/3/tv/%s' %api
-        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'es'}
-        request = requests.get(url,params=values)
+        request = requests.get(url,params=request_values)
         resp = json.loads(request.text)
         return resp
 
 def seasons_series(api,season):
         url = 'http://api.themoviedb.org/3/tv/%s/season/%s' % (api, season)
-        values = {'api_key':'ce9f97d604b836963b8de8c49437e283','language':'en'}
-        request = requests.get(url,params=values)
+        request = requests.get(url,params=request_values)
         resp = json.loads(request.text)
         return resp
