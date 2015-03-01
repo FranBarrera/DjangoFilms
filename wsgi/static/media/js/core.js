92cadd0f39be7cdc1f245045591a7e77947ed0dd
FranBarrera <franbarrera6@gmail.com>
2015-03-01 17:19:54 +0100

    corregir js

--- wsgi/static/media/js/core.js
+++ wsgi/static/media/js/core.js
@@ -6,7 +6,7 @@ var core = {
         var width_el = $('.card').find('img');
         var width_login = $('.login');
         width_el.css('height', width_el.width() * 274 / 185);
-        width_login.css('top', ($(window).height() - width_login.height()) / 2 - 100);
+        width_login.css('top', ($(window).height() - width_login.height()) / 2);
     }
 };
 
